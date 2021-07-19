532dadbb3f40712f0049ff7e74764d4a7182a69d
Manuel Aragon <aragon.manuel@live.com>
2021-07-18 23:58:08 -0500

    Added Makefile, sql_make.txt, sqLite_test.db

---
+++ Makefile
@@ -0,0 +1,6 @@
+CC=g++ 
+
+run:
+	$(CC) main.cpp -lsqlite3
+clean:
+	rm a.* *.gch
--- main.cpp
+++ main.cpp
@@ -3,11 +3,12 @@
 
 int main()
 {
-    // sqlite3 *db;
-    // sqlite3_open(
-    //     "sqLite_test.db",   /* Database filename (UTF-8) */
-    //     &db       /* OUT: SQLite db handle */
-    // );
     std::cout << "Hello World" << std::endl;
     std::cout << "Sqlite version: " << sqlite3_libversion() << std::endl;
+    sqlite3 *db;
+    sqlite3_open(
+        "sqLite_test.db",   /* Database filename (UTF-8) */
+        &db       /* OUT: SQLite db handle */
+    );
+    
 }
