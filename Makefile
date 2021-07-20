7479efd8c3f504c4b377a5977acd54041960417f
Manuel Aragon <aragon.manuel@live.com>
2021-07-20 00:20:06 -0500

    test

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-CC=g++ 
+CC=g++ -std=c++17
 
 run:
 	$(CC) main.cpp -lsqlite3
--- main.cpp
+++ main.cpp
@@ -1,14 +1,82 @@
 #include <iostream>
 #include "sqlite3.h"
+#include <filesystem>
+#include <sstream>
+#include <string>
+#include <fstream>
+
+static int createDB(const char*);
+static int createTable(const char*, const char*);
 
 int main()
 {
-    std::cout << "Hello World" << std::endl;
-    std::cout << "Sqlite version: " << sqlite3_libversion() << std::endl;
+    std::string database_name = "sqLite_test.db";
+    std::string table_file = "sql_make.txt";
+    // std::string sql_string = "CREATE TABLE IF NOT EXISTS characterTable("
+	// 	"ID INTEGER PRIMARY KEY AUTOINCREMENT, "
+	// 	"NAME      TEXT NOT NULL, "
+	// 	"Race     TEXT NOT NULL, "
+	// 	"MaxHitpoints       INT  NOT NULL, "
+	// 	"CurrentHitpoints   INT NOT NULL, "
+	// 	"ArmorClass    INT NOT NULL, "
+    //     "MovementSpeed INTEGER NOT NULL);";
+
+
     sqlite3 *db;
-    sqlite3_open(
-        "sqLite_test.db",   /* Database filename (UTF-8) */
-        &db       /* OUT: SQLite db handle */
-    );
-    
+
+    if(std::filesystem::exists(database_name))
+    {
+        std::cout << database_name << " Exists!" << std::endl;
+    }
+    else
+    {
+        std::cout << database_name << " does not exists" << std::endl;
+        createDB(database_name.c_str());
+        std::fstream table_create_stream (table_file, std::fstream::in);
+        std::string sql_line;
+        std::string sql_string;
+        while(getline(table_create_stream, sql_line))
+        {
+            std::stringstream create_string_stream(sql_line);
+            getline(create_string_stream, sql_string,';');
+        }
+        sql_string.push_back(';');
+        createTable(database_name.c_str(), sql_string.c_str());
+
+    }
+}
+
+static int createDB(const char* name)
+{
+    sqlite3 *db;
+    int exit =0;
+    exit = sqlite3_open(name, &db);
+    sqlite3_close(db);
+    std::cout << "Database " << name << " created!" << std::endl;
+    return 0;
+}
+static int createTable(const char* name, const char* sql)
+{
+    sqlite3 *db;
+    char* messageError;
+    try
+	{
+		int exit = 0;
+		exit = sqlite3_open(name, &db);
+		// An open database, SQL to be evaluated, Callback function, 1st argument to callback, Error msg written here //
+		exit = sqlite3_exec(db, sql, NULL, 0, &messageError);
+		if (exit != SQLITE_OK) {
+			std::cerr << "Error in createTable function." << std::endl;
+			sqlite3_free(messageError);
+		}
+		else
+			std::cout << "Table created Successfully" << std::endl;
+		sqlite3_close(db);
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what();
+	}
+
+	return 0;
 }
