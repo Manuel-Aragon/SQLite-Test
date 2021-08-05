4b047a293ac5448ab3bd26111c5998d8765f625e
Manuel Aragon <73243037+Manuel-Aragon@users.noreply.github.com>
2021-08-04 22:38:42 -0500

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/manuelaragon/Documents/Programming/TestProjects/SQLite-Test
-# Build directory: /Users/manuelaragon/Documents/Programming/TestProjects/SQLite-Test/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googletest")
-subdirs("test")
--- build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/manuelaragon/Documents/Programming/TestProjects/SQLite-Test/test
-# Build directory: /Users/manuelaragon/Documents/Programming/TestProjects/SQLite-Test/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(Name "DatabaseTests" "Command" "DatabaseTests")
-set_tests_properties(Name PROPERTIES  _BACKTRACE_TRIPLES "/Users/manuelaragon/Documents/Programming/TestProjects/SQLite-Test/test/CMakeLists.txt;15;add_test;/Users/manuelaragon/Documents/Programming/TestProjects/SQLite-Test/test/CMakeLists.txt;0;")
